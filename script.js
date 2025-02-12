fa5f29279141a45629a47fda583562fcb2aac55f
Kaua Gomes Pereira <113049135+kauagg@users.noreply.github.com>
2025-02-12 17:00:53 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const loginsButton = document.querySelector('.login-btn');
+const backButton = document.querySelector('.login-btn');
+
+
+
+backButton.addEventListener('click', () => {
+    window.location.href = "index.html"
+});
+
+loginButton.addEventListener ('click', () => {
+     window.location.href = "index.html"
+})
